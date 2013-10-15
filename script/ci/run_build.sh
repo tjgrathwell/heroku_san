df21171598d6e7ae98bf1f7ccbba1c04d931e0db
Colin O'Byrne and Travis Grathwell <pair+cobyrne+tgrathwell@pivotallabs.com>
2013-10-14 17:53:59 -0700

    Print out some secret ENV vars for fun

--- script/ci/run_build.sh
+++ script/ci/run_build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+echo "I know all your secret!"
+echo $POTATO
+echo $SNACKS
+
 git config --global user.email "travis-ci@example.com"
 git config --global user.name "Travis CI"
 
